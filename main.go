c412cb81ab978f92658d129f0052c3a2b93f3485
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-10 04:38:28 +0300

    restrict home handler to accept requests on path '/'

--- main.go
+++ main.go
@@ -18,6 +18,10 @@ func main() {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("hello from Snipperbox"))
 }
 
