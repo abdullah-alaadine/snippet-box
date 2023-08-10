75d49fc85353a0282d88e08ad7b18443bde89453
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-10 04:25:21 +0300

    create a web server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+
+	log.Printf("starting server on port :%d", 4000)
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from Snipperbox"))
 }
