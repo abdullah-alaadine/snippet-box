ed773cf722a3b003a884e86ee807f9af21917a02
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-11 04:00:06 +0300

    get the id from url query params

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -26,7 +28,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "display a specific snippet with ID: %d", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
