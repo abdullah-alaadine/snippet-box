b78038375ad9ae6f42ab86d9dbd8a91506df1972
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-21 22:24:09 +0300

    initialize new formDecoder

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"github.com/abdullah-alaadine/snippet-box/internal/models"
+	"github.com/go-playground/form"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -18,6 +19,7 @@ type application struct {
 	infoLog       *log.Logger
 	snippets      *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
 }
 
 func main() {
@@ -42,11 +44,14 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		infoLog:       infoLog,
 		errorLog:      errorLog,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder:   formDecoder,
 	}
 
 	srv := &http.Server{
