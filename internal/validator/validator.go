d082f512862dec70022f2e0c0b6b4654163b8791
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-21 21:58:35 +0300

    add CheckField method

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -16,3 +16,9 @@ func (v *Validator) AddFieldError(key, message string) {
 		v.FieldErrors[key] = message
 	}
 }
+
+func (v *Validator) CheckField(ok bool, key, message string) {
+	if !ok {
+		v.AddFieldError(key, message)
+	}
+}
