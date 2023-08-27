6de8e2d9cf5f39ead39e7fc9158225bdd3381302
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-27 15:06:58 +0300

    create userSignupForm struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -98,6 +98,13 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
 
+type userSignupForm struct {
+	Name                string `form:"name"`
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Display a HTML form for signing up a new user...")
 }
