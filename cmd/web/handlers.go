99a4c78a124188c7d759f64a804ddc521d02899c
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-27 15:08:44 +0300

    update userSignup handler to render the signup form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -106,7 +106,9 @@ type userSignupForm struct {
 }
 
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Display a HTML form for signing up a new user...")
+	data := app.newTemplateData(r)
+	data.Form = userSignupForm{}
+	app.render(w, http.StatusOK, "signup.tmpl", data)
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
