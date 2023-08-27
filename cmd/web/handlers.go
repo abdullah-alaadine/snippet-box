45520f020fc533e290a94baa73d47c83cb161207
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-27 16:31:45 +0300

    create userLoginForm struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -150,6 +150,12 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
+type userLoginForm struct {
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Display a HTML form for logging in a user...")
 }
