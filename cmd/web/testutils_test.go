eca8b8b9f2bfc21a19c243dba0dc5c2c1435f51d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-11-09 12:10:19 +0200

    create a get helper func to send http get request while testing

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"log"
 	"net/http"
@@ -22,4 +23,18 @@ type testServer struct {
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
 	return &testServer{ts}
+}
+
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
+	return rs.StatusCode, rs.Header, string(body)
 }
