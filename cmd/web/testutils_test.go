d3a71a02eb3972c0090753a430862330c8171f02
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-11-09 12:07:04 +0200

    create newTestApplication helper function

---
+++ cmd/web/testutils_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"io"
+	"log"
+	"testing"
+)
+
+func newTestApplication(t *testing.T) *application {
+	return &application{
+		errorLog: log.New(io.Discard, "", 0),
+		infoLog:  log.New(io.Discard, "", 0),
+	}
+}
